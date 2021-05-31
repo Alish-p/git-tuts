0cf83b46ff200632133baeaced77c37a82a45b44
Alish Palasara <palasaraalis@gmail.com>
2021-05-31 23:05:14 +0530

    Added ServerJs and edited index

--- index.js
+++ index.js
@@ -2,15 +2,5 @@ function square(number) {
   if (number) return number * number;
 }
 
-// console.log(0 == undefined);
-// console.log(undefined === null);
-
-console.log('%cobject', 'color:yellow');
-
-console.log(square());
-console.log(square(0));
-console.log(square(''));
-console.log(square({}));
-// x(undefined);
-// x(null);
-// x('12345');
+console.log(0 == undefined);
+console.log(undefined === null);
