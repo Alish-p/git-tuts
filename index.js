375df16019a386e30aff78aecaaa3b48780e8f0b
Alish Palasara <palasaraalis@gmail.com>
2021-05-31 22:52:54 +0530

    index.js file added

---
+++ index.js
@@ -0,0 +1,16 @@
+function square(number) {
+  if (number) return number * number;
+}
+
+// console.log(0 == undefined);
+// console.log(undefined === null);
+
+console.log('%cobject', 'color:yellow');
+
+console.log(square());
+console.log(square(0));
+console.log(square(''));
+console.log(square({}));
+// x(undefined);
+// x(null);
+// x('12345');
